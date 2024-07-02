a770581e9f3303c25840689bc02e4e595eaced45
kyleerickson88 <kyle88erickson@gmail.com>
2024-07-02 17:03:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import base64
+import matplotlib.pyplot as plt
+import seaborn as sns
+from bs4 import BeautifulSoup
+from bs4 import Comment
+from PIL import Image
+import requests
+import re
+import plotly as py
+import plotly.graph_objects as go
+import plotly.express as px
+import altair as alt
 
+st.set_page_config(page_title='Hey Batter!', page_icon=':baseball bat:', layout="wide")
